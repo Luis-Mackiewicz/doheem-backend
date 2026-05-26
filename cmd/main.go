bb638bafffdbcb3eb4f80caa8693758993c26846
Luis Miguel Mackiewicz <luismiguelpix@gmail.com>
2026-05-26 16:26:09 -0300

    feat: create mux server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Doheem server is running! 🚀")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", indexHandler)
+
+	fmt.Println("Doheem server is running! 🚀")
+
+	http.ListenAndServe(":8080", mux)
 }
